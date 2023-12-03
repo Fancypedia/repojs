90b1b256196d36b84955cdb4c35caf646f9b0c8b
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-12-03 12:20:29 +0700

    STATUS

--- inijs/content/createproduct.js
+++ inijs/content/createproduct.js
@@ -53,7 +53,7 @@ export default function buatPRODUK(){
         "Size": getValue("size"),
         "Stock": parseInt(getValue("stock")),
         "Image": getValue("image"),
-        "Status": true
+        "Status": getValue("status"),
     }
     
     postBiasa(createproductfix,datainjson,tokenkey,tokenvalue,responseData);
