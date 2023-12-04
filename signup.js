460b6c9e754f31b23f73547b76675868b99350a5
Maulana Imanulhaq <imanulhaq5@gmail.com>
2023-12-04 14:06:17 +0700

    signup disable

--- signup.js
+++ signup.js
@@ -3,8 +3,12 @@ import { getValue } from "./element.js";
 import { userbarunowa } from "./gudangAPI.js";
 
 const loadingIndicator = document.getElementById("loadingIndicator");
+const loginButton = document.getElementById("btnSignup");
 
 export default function RegistrasiUser(){
+    // Disable the login button to prevent multiple clicks
+    loginButton.disabled = true;
+
     let username = getValue("usernamesignup");
     let password = getValue("passwordsignup");
     let nowa = getValue("nowa");
@@ -20,6 +24,10 @@ export default function RegistrasiUser(){
         alert("NoWa perlu diisi");
         return; // Stop execution if the fields are not filled
     }
+
+    // Disable the login button to prevent multiple clicks
+    loginButton.disabled = true;
+
     let datainjson = {
         "username": username,
         "password": password,
@@ -32,6 +40,9 @@ export default function RegistrasiUser(){
 
 function responseData(result) {
     loadingIndicator.style.display = "none";
+
+    // Enable the login button regardless of the result
+    loginButton.disabled = false;
 
     if (result.message == "Berhasil Input data") {
         alert("Berhasil Input Data");
