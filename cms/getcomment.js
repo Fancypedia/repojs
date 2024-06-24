6a3245a50e5e8344552bc07f638fe8151119828d
Maulana Imanulhaq <imanulhaq5@gmail.com>
2024-06-24 23:19:03 +0700

    getCommentAPI

--- cms/getcomment.js
+++ cms/getcomment.js
@@ -1,6 +1,6 @@
 // Fetch all products from the server
 function getComment() {
-  fetch('https://asia-southeast2-testlogin-366704.cloudfunctions.net/getallcomment')
+  fetch('https://asia-southeast2-testlogin-366704.cloudfunctions.net/getall-comment')
     .then(response => response.json())
     .then(data => {
       if (data.status === true) {
